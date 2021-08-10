b06a4affc925bbecbcc47342d41e2f12f604ad3b
Silentintruder <pkkumar.prateek731@gmail.com>
2021-08-10 23:19:21 +0530

    Register

--- OnlineExam/ExamApp/src/app/app-routing.module.ts
+++ OnlineExam/ExamApp/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {
@@ -9,6 +10,9 @@ const routes: Routes = [
   },
   {
     path: 'loginLink', component: LoginComponent
+  },
+  {
+    path: 'registerLink', component: RegisterComponent
   }
 ];
 
