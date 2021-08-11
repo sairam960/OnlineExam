11c8f421ccb35ff690bfe4e07a362874e0f99bdb
deeksha-raghu <raghudeeksha@gmail.com>
2021-08-11 14:34:30 +0530

    commit

--- OnlineExam/src/main/java/com/lti/model/Exam.java
+++ OnlineExam/src/main/java/com/lti/model/Exam.java
@@ -2,6 +2,7 @@ package com.lti.model;
 
 public class Exam {
 	private int examId;
+	private int subjectId;
 	private int marks;
 	private int level;
 
--- OnlineExam/src/main/java/com/lti/model/Response.java
+++ OnlineExam/src/main/java/com/lti/model/Response.java
@@ -1,5 +1,12 @@
 package com.lti.model;
 
 public class Response {
-
+	private int responseId;
+	private String studentId;
+	private String subjectName;
+	private int level;
+	private int score;
+	private Exam exam;
+	
+	
 }
