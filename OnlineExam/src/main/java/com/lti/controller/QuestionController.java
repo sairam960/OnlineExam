0bdac021050c1dc2113e681cea252182287b71b0
sairam960 <ksairamnath@gmail.com>
2021-08-12 02:23:09 +0530

    Added CrossOrigin!

--- OnlineExam/src/main/java/com/lti/controller/QuestionController.java
+++ OnlineExam/src/main/java/com/lti/controller/QuestionController.java
@@ -3,6 +3,7 @@ package com.lti.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import com.lti.model.Questions;
 import com.lti.service.QuestionService;
 
 @RestController
+@CrossOrigin(origins="http://localhost:4200")
 @RequestMapping("/questiondata/api")
 public class QuestionController {
 	
--- OnlineExam/src/main/java/com/lti/controller/StudentController.java
+++ OnlineExam/src/main/java/com/lti/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.lti.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import com.lti.model.StudentData;
 import com.lti.service.StudentService;
 
 @RestController
+@CrossOrigin(origins="http://localhost:4200")
 @RequestMapping("/studentdata/api")
 public class StudentController {
 	
