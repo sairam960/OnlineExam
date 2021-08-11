c19659cdc86a694f1a3d046b460788548935a057
SrishtiRai2704 <srishtirai2704@gmail.com>
2021-08-11 17:09:50 +0530

    ClearedLevel

---
+++ OnlineExam/ExamApp/src/app/next-level/next-level.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-next-level',
+  templateUrl: './next-level.component.html',
+  styleUrls: ['./next-level.component.css']
+})
+export class NextLevelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
