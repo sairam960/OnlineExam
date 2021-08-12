1a9d400e1cd9afb564ed795a3d4fdaddf18d8c14
SrishtiRai2704 <srishtirai2704@gmail.com>
2021-08-12 12:00:00 +0530

    NotClear

--- OnlineExam/ExamApp/src/app/app-routing.module.ts
+++ OnlineExam/ExamApp/src/app/app-routing.module.ts
@@ -12,6 +12,8 @@ import { SearchComponent } from './search/search.component';
 import { StudentLoginComponent } from './student-login/student-login.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 
+import { NotClearComponent } from './not-clear/not-clear.component';
+import { NextLevelComponent } from './next-level/next-level.component';
 const routes: Routes = [
   {
     path: '', component: HomeComponent
@@ -30,6 +32,12 @@ const routes: Routes = [
   },
   {
     path: 'addComponent', component:AddComponent
+  },
+  {
+    path: 'notclear',component:NotClearComponent
+  },
+  {
+    path:'nextlevel',component:NextLevelComponent
   },
   {path:'JavaTest',component:InstructionComponent},
   {path:'DBMSTest',component:InstructionComponent},
