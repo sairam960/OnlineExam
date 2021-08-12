579f6f54086b72ebf4cd23f260c305bf52df474d
Silentintruder <pkkumar.prateek731@gmail.com>
2021-08-12 11:01:20 +0530

    Register done

--- OnlineExam/ExamApp/src/app/register/register.component.ts
+++ OnlineExam/ExamApp/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { StudentData } from '../student-data';
 import { StudentdataService } from '../studentdata.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -11,7 +12,7 @@ export class RegisterComponent implements OnInit {
 
   stud:StudentData;
 
-  constructor(private studservice:StudentdataService) { 
+  constructor(private studservice:StudentdataService, private router:Router) { 
     this.stud=new StudentData();
   }
 
@@ -19,11 +20,13 @@ export class RegisterComponent implements OnInit {
   }
 
   savedata(){
-
     this.studservice.addNewStudent(this.stud).subscribe(
       (data)=>{
         console.log("return Value from REST"+data)
       }
-    )
+    )  
+    alert("Student Register!, Please click OK and Proceed to Login")
+    this.router.navigate(['/student-loginLink']);
   }
+  
 }
