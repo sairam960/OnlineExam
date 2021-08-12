0d4d2395d98a975f7ba565af13201a23e200c58c
Silentintruder <pkkumar.prateek731@gmail.com>
2021-08-12 13:09:17 +0530

    Main changes

--- OnlineExam/ExamApp/src/app/admin-login/admin-login.component.ts
+++ OnlineExam/ExamApp/src/app/admin-login/admin-login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-login',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminLoginComponent implements OnInit {
 
-  constructor() { }
+  userName:string;
+  password:string;
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
+  checkadmin(){
+    
+    if(this.userName=="Admin" && this.password=="Admin@123"){
+      this.router.navigate(['/homeLink']);
+      sessionStorage.setItem("uname", this.userName);
 
+    }else{
+    this.router.navigate(['/loginLink']);
+    }
+  }
 }
