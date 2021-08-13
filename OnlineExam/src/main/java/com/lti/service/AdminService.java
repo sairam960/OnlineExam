69a9ac41707ee49b5f0e3f9ac2a3b49fbfc01d8d
sairam960 <ksairamnath@gmail.com>
2021-08-13 11:53:26 +0530

    Student login validation

---
+++ OnlineExam/src/main/java/com/lti/service/AdminService.java
@@ -0,0 +1,7 @@
+package com.lti.service;
+
+public interface AdminService {
+	
+	public boolean adminLogin(String email,String password);
+
+}
---
+++ OnlineExam/src/main/java/com/lti/service/AdminServiceImpl.java
@@ -0,0 +1,11 @@
+package com.lti.service;
+
+public class AdminServiceImpl implements AdminService{
+
+	@Override
+	public boolean adminLogin() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
