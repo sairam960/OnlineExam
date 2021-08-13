2a3b8fed466de28662bff3d88395a28e9224dafa
SrishtiRai2704 <srishtirai2704@gmail.com>
2021-08-13 12:06:18 +0530

    Update ResponseController.java

--- OnlineExam/src/main/java/com/lti/controller/ResponseController.java
+++ OnlineExam/src/main/java/com/lti/controller/ResponseController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lti.model.Response;
-import com.lti.model.Subject;
 import com.lti.service.ResponseService;
 
 @RestController
