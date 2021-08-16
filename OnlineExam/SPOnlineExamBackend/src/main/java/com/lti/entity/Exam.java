2e67bfe81614426747aefd9c8cd2432df649f063
sairam960 <ksairamnath@gmail.com>
2021-08-17 03:13:58 +0530

    Last commit

--- OnlineExam/SPOnlineExamBackend/src/main/java/com/lti/entity/Exam.java
+++ OnlineExam/SPOnlineExamBackend/src/main/java/com/lti/entity/Exam.java
@@ -2,8 +2,6 @@ package com.lti.entity;
 
 import java.time.LocalDateTime;
 import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
