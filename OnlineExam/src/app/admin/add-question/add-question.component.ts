17c39427e01c9869d4114a1100b2357cbadc2210
sairam960 <ksairamnath@gmail.com>
2021-08-17 01:11:09 +0530

    csv files working

--- OnlineExam/src/app/admin/add-question/add-question.component.ts
+++ OnlineExam/src/app/admin/add-question/add-question.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Question } from 'src/app/appmodel/Question';
-import { Subject } from 'src/app/appmodel/Subject';
-import { UserService } from 'src/app/user.service';
+import { Question } from '../../appmodel/Question';
+import { Subject } from '../../appmodel/Subject';
+import { UserService } from '../../user.service';
 import { AdminService } from '../admin.service';
+import { NgxCsvParser, NgxCSVParserError } from 'ngx-csv-parser';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-add-question',
@@ -12,25 +14,52 @@ import { AdminService } from '../admin.service';
 })
 export class AddQuestionComponent {
 
-  constructor(private adminService:AdminService,private userService:UserService,private router: Router) { }
+  constructor(private ngxCsvParser: NgxCsvParser,private adminService:AdminService,private userService:UserService,private router: Router) { }
 
   question:Question=new Question();
   subject:Subject=new Subject();
   subjects:Subject[]=[];
   imageurl:String='/assets/images/'; 
   result:any=[];
+  csvRecords: any[] = [];
+  header: boolean = true;
   
   ngOnInit():void{
     this.getSubjects();
   }
+
+  fileChangeListener($event: any): void {
+
+    const files = $event.srcElement.files;
+    this.header = (this.header as unknown as string) === 'true' || this.header === true;
+    this.ngxCsvParser.parse(files[0], { header: this.header, delimiter: '|' })
+      .pipe().subscribe((result: any) => {
+        console.log('Result', result);
+        this.csvRecords = result;
+      }, (error: NgxCSVParserError) => {
+        console.log('Error', error);
+      });
+  } 
   addQuestion(){
     alert(JSON.stringify(this.question));
     this.question.subject.name="Java";
     this.question.subject.id=100;
-    
+
+    this.csvRecords.forEach(element => {
+      this.question.question=element.question;
+      this.question.option1=element.option1;
+      this.question.option2=element.option2;
+      this.question.option3=element.option3;
+      this.question.option4=element.option4;
+      this.question.answer=element.answer;
+      console.log(this.question)
       this.adminService.addQuestion(this.question).subscribe(response=>{
         alert(JSON.stringify(response));
     })
+    });
+      //this.adminService.addQuestion(this.question).subscribe(response=>{
+        //alert(JSON.stringify(response));
+   // })
   }
 
   getSubjects(){
