b72d574d653cc82df3dd08f8c27754512b9197d7
Silentintruder <pkkumar.prateek731@gmail.com>
2021-08-17 09:47:58 +0530

    minor changes

--- OnlineExam/SPOnlineExamBackend/src/main/java/com/lti/exception/AdminServiceException.java
+++ OnlineExam/SPOnlineExamBackend/src/main/java/com/lti/exception/AdminServiceException.java
@@ -4,23 +4,22 @@ public class AdminServiceException extends RuntimeException{
 
 	public AdminServiceException() {
 		super();
-		// TODO Auto-generated constructor stub
+		
 	}
-
 
 	public AdminServiceException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public AdminServiceException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public AdminServiceException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	
--- OnlineExam/SPOnlineExamBackend/src/main/java/com/lti/exception/UserServiceException.java
+++ OnlineExam/SPOnlineExamBackend/src/main/java/com/lti/exception/UserServiceException.java
@@ -5,24 +5,24 @@ public class UserServiceException extends RuntimeException {
 	
 	public UserServiceException() {
 		super();
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public UserServiceException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	
 	public UserServiceException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	 
 	public UserServiceException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
+	
 	}
 	
 	
